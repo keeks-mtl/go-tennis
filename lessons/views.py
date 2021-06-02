4bc62602b674cd2641d482d69689d719cdfe8b05
keeks-mtl <wallarand@gmail.com>
2021-06-02 16:03:34 +0000

    Add lessons tmeplates, urls and views

--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from .models import Lesson, ClassType
 
 # Create your views here.
+
+
+def lessons(request):
+    """ A view to return the index page """
+
+    return render(request, 'lessons/lessons.html')
