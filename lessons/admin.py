ffb62552caf513d6cd9595723e6555f35e9b0b12
keeks-mtl <wallarand@gmail.com>
2021-06-02 14:55:18 +0000

    Create lessons app & models

---
+++ lessons/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import ClassType, Lesson
+
+# Register your models here.
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'price',
+        'coach',
+        'date',
+        'spots',
+        'students',
+    )
+
+    ordering = ('date',)
+
+
+class ClassTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(ClassType, ClassTypeAdmin)
---
+++ lessons/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class ClassType(models.Model):
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Lesson(models.Model):
+    class_type = models.ForeignKey(
+        'ClassType', null=True, blank=True, on_delete=models.SET_NULL)
+    id = models.CharField(primary_key=True, max_length=40, null=False, blank=False)
+    coach = models.CharField(max_length=30, null=True, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2)
+    date = models.DateField(null=True, blank=True)
+    time = models.TimeField(null=True, blank=True)
+    spots = models.IntegerField(default=4, null=True, blank=True)
+    students = models.CharField(max_length=30, null=True, blank=True)
+
+    def __str__(self):
+        return self.id
