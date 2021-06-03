486736f0116342c6f1627add195aea8605bb2467
keeks-mtl <wallarand@gmail.com>
2021-06-03 20:11:56 +0000

    Add book_lesson and remove_lesson urls to bag app urls

--- bag/urls.py
+++ bag/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
     path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
     path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
+    path('book_lesson/<item_id>/', views.book_lesson, name='book_lesson'),
+    path('remove_lesson/<item_id>/', views.remove_lesson, name='remove_lesson'),
 ]
