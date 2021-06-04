87e0d59be58c8a0d133cb4a8b62820da36d41a1d
keeks-mtl <wallarand@gmail.com>
2021-06-04 21:51:04 +0000

    Finish coaches template and view

--- coaches/views.py
+++ coaches/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+from .models import Coach
+
 # Create your views here.
 
 
 def view_coaches(request):
     """ A view to return the coaches page """
 
-    return render(request, 'coaches/coaches.html')
+    coaches = Coach.objects.all()
+
+    context = {
+        'coaches': coaches,
+    }
+    return render(request, 'coaches/coaches.html', context)
