7f80d9e7a0d897dbc89310eea9c27a3825825fe6
keeks-mtl <wallarand@gmail.com>
2021-07-01 14:00:28 +0000

    Create comment model in coaches

--- coaches/models.py
+++ coaches/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -23,3 +24,17 @@ class Coach(models.Model):
 
     def __str__(self):
         return self.first_name
+
+
+class Comment(models.Model):
+    """
+    A comment model
+    """
+    coach = models.ForeignKey(Coach, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    stars = models.IntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return f"Comment about {self.coach.first_name} by {self.author}"
