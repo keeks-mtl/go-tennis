64838780fe648afcee1a20c47deb935947ebb3b5
keeks-mtl <wallarand@gmail.com>
2021-07-07 11:29:38 +0000

    Change naming of variables in javascript files

--- static/js/main.js
+++ static/js/main.js
@@ -3,32 +3,32 @@ $(document).ready(function(){
     // $("#delete-modal").on("show.bs.modal", function(event){
         
     //     //Get the button that triggered the modal
-    //     var button = $(event.relatedTarget);
+    //     let button = $(event.relatedTarget);
 
     //     // Extract value from custon data-name attribute to link coach_first_name
-    //     var name = button.data("name");
+    //     let name = button.data("name");
     //     $(this).find("#coach-name").text(name);
         
     //     // Extract value from the custom data-* attribute to link the coach_id 
-    //     var url = button.data("url");
+    //     let url = button.data("url");
     //     $(this).find('#confirm-delete').attr('href', url);
     // });
     // delete modal function
     $("#delete-modal").on("show.bs.modal", function(event){
         
         //Get the button that triggered the modal
-        var button = $(event.relatedTarget);
+        let button = $(event.relatedTarget);
 
         // Extract value from custon data-topic attribute to link delete topic
-        var topic = button.data("topic");
+        let topic = button.data("topic");
         $(this).find("#delete-topic").text(topic);
 
         // Extract value from custon data-name attribute to link topic name
-        var name = button.data("name");
+        let name = button.data("name");
         $(this).find("#topic-name").text(name);
         
         // Extract value from the custom data-* attribute to link the coach_id 
-        var url = button.data("url");
+        let url = button.data("url");
         $(this).find('#confirm-delete').attr('href', url);
     });
 });
