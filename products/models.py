8f7533fc246bd4882845787909eebe02696a6f53
keeks-mtl <wallarand@gmail.com>
2021-07-07 13:16:51 +0000

    Fix form validation on add product

--- products/models.py
+++ products/models.py
@@ -19,13 +19,13 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    sku = models.CharField(max_length=254)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(
         max_digits=6, decimal_places=2)
     rating = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True)
+        max_digits=6, decimal_places=2, null=False, blank=False)
     image = models.ImageField(null=True, blank=True)
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
 
