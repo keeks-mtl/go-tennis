39ae50c6a73dd57eb8ce5162c0f23e72513ec9e7
keeks-mtl <wallarand@gmail.com>
2021-07-08 23:02:49 +0000

    Change phone_number to not required on coaches models

--- coaches/models.py
+++ coaches/models.py
@@ -15,13 +15,16 @@ class Coach(models.Model):
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)    
+    phone_number = models.CharField(max_length=20, null=True, blank=True)    
     description = models.TextField()
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
+        return self.first_name
+
+    def get_first_name(self):
         return self.first_name
 
 
