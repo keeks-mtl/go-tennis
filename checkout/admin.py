27b9de1d5334dd688f4f6ef5ceef044cbb48165b
keeks-mtl <wallarand@gmail.com>
2021-07-09 03:38:10 +0000

    Add original lesson bag to admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderLineItem
+from .models import Order, OrderLineItem, LessonLineItem
 
 
 class OrderLineItemAdminInline(admin.TabularInline):
@@ -7,24 +7,30 @@ class OrderLineItemAdminInline(admin.TabularInline):
     readonly_fields = ('lineitem_total',)
 
 
+class LessonLineItemAdminInline(admin.TabularInline):
+    model = LessonLineItem
+    readonly_fields = ('lesson_lineitem_total',)
+
+
 class OrderAdmin(admin.ModelAdmin):
-    inlines = (OrderLineItemAdminInline,)
+    inlines = (OrderLineItemAdminInline, )
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total', 'original_bag',
-                       'stripe_pid')
+                       'lesson_total', 'grand_total',
+                       'original_bag', 'stripe_pid',
+                       'original_lesson_bag')
 
     fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total', 'original_bag',
-              'stripe_pid')
+              'order_total', 'lesson_total', 'grand_total', 'original_bag',
+              'stripe_pid', 'original_lesson_bag')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
-                    'grand_total',)
+                    'lesson_total', 'grand_total',)
 
     ordering = ('-date',)
 
