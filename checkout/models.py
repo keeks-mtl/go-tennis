1ed5f401364c0efdb7e06beb7a28af19a97a883d
keeks-mtl <wallarand@gmail.com>
2021-07-09 03:37:19 +0000

    Add lesson_total and original lesson bag to checkout model

--- checkout/models.py
+++ checkout/models.py
@@ -27,8 +27,10 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    lesson_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
+    original_lesson_bag = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
@@ -43,11 +45,13 @@ class Order(models.Model):
         accounting for delivery costs.
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.lesson_total = self.lessonlineitems.aggregate(Sum('lesson_lineitem_total'))['lesson_lineitem_total__sum'] or 0
+
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
-        self.grand_total = self.order_total + self.delivery_cost
+        self.grand_total = self.order_total + self.delivery_cost + self.lesson_total
         self.save()
 
     def save(self, *args, **kwargs):
@@ -89,7 +93,7 @@ class LessonLineItem(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the lineitem total
+        Override the original save method to set the lesson lineitem total
         and update the order total.
         """
         self.lesson_lineitem_total = self.lesson.price
