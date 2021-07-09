d18adf22d374b2420eaa4dc7bea0c6d89b5c92a6
keeks-mtl <wallarand@gmail.com>
2021-07-09 03:39:31 +0000

    Try changing checkout signals

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,14 +1,20 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from .models import OrderLineItem
+from .models import OrderLineItem, LessonLineItem
 
-@receiver(post_save, sender=OrderLineItem)
-def update_on_save(sender, instance, created, **kwargs):
-    """
-    Update order total on lineitem update/create
-    """
+def update_on_save(sender,instance, **kwargs):
     instance.order.update_total()
+
+post_save.connect(update_on_save, sender=OrderLineItem)
+# post_save.connect(update_on_save, sender=LessonLineItem)
+
+# @receiver(post_save, sender=OrderLineItem)
+# def update_on_save(sender, instance, created, **kwargs):
+#     """
+#     Update order total on lineitem update/create
+#     """
+#     instance.order.update_total()
 
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
