935cc8cd97aa0a08e99eb4fed6f5017a8092f49d
keeks-mtl <wallarand@gmail.com>
2021-07-09 18:24:12 +0000

    Remove secret key and set debug

--- go_tennis/settings.py
+++ go_tennis/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q@relt@f5e^(tq4=uhob$7b)f&%uks*^%egbz1z-3r!)nv9x+i'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['go-tennis.herokuapp.com', 'localhost']
 
