75709a8f808bafe46285c46add7db240942360b7
keeks-mtl <wallarand@gmail.com>
2021-07-11 17:30:20 +0000

    Remove comments

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,4 @@
 $(document).ready(function(){
-    // delete coach modal function
-    // $("#delete-modal").on("show.bs.modal", function(event){
-        
-    //     //Get the button that triggered the modal
-    //     let button = $(event.relatedTarget);
-
-    //     // Extract value from custon data-name attribute to link coach_first_name
-    //     let name = button.data("name");
-    //     $(this).find("#coach-name").text(name);
-        
-    //     // Extract value from the custom data-* attribute to link the coach_id 
-    //     let url = button.data("url");
-    //     $(this).find('#confirm-delete').attr('href', url);
-    // });
-    // delete modal function
     $("#delete-modal").on("show.bs.modal", function(event){
         
         //Get the button that triggered the modal
