508917af644468a1b1a7d019bd459317ec8abb60
keeks-mtl <wallarand@gmail.com>
2021-07-14 01:08:09 +0000

    Style products templates on app and change product model

--- products/models.py
+++ products/models.py
@@ -6,8 +6,8 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "Categories"
 
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, blank=True)
+    name = models.CharField(max_length=50)
+    friendly_name = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return self.name
@@ -19,8 +19,8 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254)
-    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(
         max_digits=6, decimal_places=2)
