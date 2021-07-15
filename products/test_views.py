565af4fb0621156abbfbe3681319fdfe969d14c2
keeks-mtl <wallarand@gmail.com>
2021-07-15 20:37:31 +0000

    Create test_views for product app

---
+++ products/test_views.py
@@ -0,0 +1,42 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.messages import get_messages
+from products.models import Category, Product
+
+
+class TestProductViews(TestCase):
+    "test the stock views for all users"
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='testpassword'
+        )
+        self.category = Category.objects.create(
+            name="test_category",
+            friendly_name="Test Category"
+        )
+        self.product = Product.objects.create(
+            category=self.category,
+            sku="1",
+            name="test product",
+            description="test description",
+            price="2.99",
+            rating="4",
+            image="testimage.jpg",
+            has_sizes=False,
+        )
+        self.products = reverse("products")
+        self.product_detail = reverse("product_detail",
+                                   kwargs={"product_id": self.product.id})
+
+    def test_products_view(self):
+        ''' Test the products view '''
+
+        response = self.client.get(self.products)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products/products.html")
+        self.assertTemplateUsed(response, "base.html")
