ee23990d681909176cabcb8e10231d0208fb13ad
keeks-mtl <wallarand@gmail.com>
2021-07-16 17:00:07 +0000

    Fix signal bug with new receiver on checkout signal

--- checkout/signals.py
+++ checkout/signals.py
@@ -4,24 +4,12 @@ from django.dispatch import receiver
 from .models import OrderLineItem, LessonLineItem
 
 
-def update_on_save(sender, instance, **kwargs):
-    instance.order.update_total()
-
-
-post_save.connect(update_on_save, sender=OrderLineItem)
-# post_save.connect(update_on_save, sender=LessonLineItem)
-
-# @receiver(post_save, sender=OrderLineItem)
-# def update_on_save(sender, instance, created, **kwargs):
-#     """
-#     Update order total on lineitem update/create
-#     """
-#     instance.order.update_total()
-
-
+@receiver(post_save, sender=LessonLineItem)
+@receiver(post_save, sender=OrderLineItem)
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_delete(sender, instance, **kwargs):
+@receiver(post_delete, sender=LessonLineItem)
+def update_on_save(sender, instance, created, **kwargs):
     """
-    Update order total on lineitem delete
+    Update order total on lineitem & lessonlineitem update/create/delete
     """
     instance.order.update_total()
