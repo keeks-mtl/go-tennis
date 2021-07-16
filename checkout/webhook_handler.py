ecd855d02a8d0a1f0ca067bebcf7db89ac60aada
keeks-mtl <wallarand@gmail.com>
2021-07-16 16:57:04 +0000

    Add lessonlineitem and original_lesson_bag to webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
 
-from .models import Order, OrderLineItem
+from .models import Order, OrderLineItem, LessonLineItem
 from products.models import Product
 from lessons.models import Lesson
 from profiles.models import UserProfile
@@ -98,6 +98,7 @@ class StripeWH_Handler:
                     county__iexact=shipping_details.address.state,
                     grand_total=grand_total,
                     original_bag=bag,
+                    original_lesson_bag=lesson_bag,
                     stripe_pid=pid,
                 )
                 order_exists = True
@@ -125,6 +126,7 @@ class StripeWH_Handler:
                     street_address2=shipping_details.address.line2,
                     county=shipping_details.address.state,
                     original_bag=bag,
+                    original_lesson_bag=lesson_bag,
                     stripe_pid=pid,
                 )
                 for item_id, item_data in json.loads(bag).items():
